498285b29caab65cc7c805c504905cb772baa1bc
Ayush Verma <ayush.k1998@gmail.com>
2019-03-27 01:11:18 +0530

    first finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
